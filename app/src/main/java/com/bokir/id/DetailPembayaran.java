9df11588ea851365de5bf1763bbabf9a427ae6c4
AnonHunterID <garuda_cyber@yahoo.co.id>
2019-12-17 00:51:35 +0700

    update

--- app/src/main/java/com/bokir/id/DetailPembayaran.java
+++ app/src/main/java/com/bokir/id/DetailPembayaran.java
@@ -3,8 +3,17 @@ package com.bokir.id;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class DetailPembayaran extends AppCompatActivity {
+
+    TextView jdl,alm,waktu,free,freeint;
+    Button btn;
+    public String KEY_JDL = "judul";
+    public String KEY_ALM = "judul";
+    public String KEY_WKT = "judul";
+    public String KEY_FREE = "judul";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +22,9 @@ public class DetailPembayaran extends AppCompatActivity {
 
         getSupportActionBar().setTitle("Detail Pembayaran");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+
+
     }
 }
